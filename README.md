10199ea31134a73da652f995137c96c11e77180d
examensfsg <examensfsg@gmail.com>
2021-10-23 11:32:23 -0400

    add license and readme

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # Examens FSG
 
-WIP
+Banque d'examen non officielle pour la Faculté de Sciences et de Génie de l'Université Laval.
+Le site Web est accessible à l'adresse suivante:
+- https://examensfsg.github.io/examens/
+
+## Gestion de la base de données
+
+La base de données peut être gérée avec le programme situé dans le dossier `dbhelper`.
+Le programme comporte une page d'aide qui liste les commandes permises.
+```
+dbhelper/main.py --help
+```
+
+## Demande de retrait
+
+Pour faire retirer un examen de la base de données, veuillez remplir [ce formulaire][contact-form].
+L'historique des versions des examens n'est pas suivi avec Git.
+Veuillez noter cependant que les examens resteront disponibles dans les forks si jamais il y en a.
+
+## Licence
+
+Le code du site Web et de gestion de la base de données est distribué sous la license Apache 2.0.
+Les droits sur les examens appartiennent à leur auteur exclusivement.
+
+
+[contact-form]: https://forms.gle/XwmN4mxGK5nUCNp27
---
+++ publish.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+# we want to avoid tracking exam PDFs in git
+# in order to do this, exams are kept in a separate branch from the rest,
+# which contains a single commit that gets continuously amended.
+# the database branch is used to publish to github pages.
+
+# we don't want to keep exam history because:
+# - exams takes a lot of space and github has limited storage space
+# - eventual copyright takedown requests must be fulfilled,
+#     and history would have to be cleared (although nothing can be done for forks...)
+
+# also: git LFS doesn't work with github pages, but that doesn't matter
+# since there's normally no history stored for the PDFs.
+
+git switch database
+git merge master --squash -X theirs   # note that deleted files on master will still be present!
+# --> ./main.py add ...
+git add exam/ db/
+
+git commit -m "database update"
+# if commit is amended, github will not diff objects correctly and the whole thing is reuploaded!
+# so only change history once in a while, squashing commits.
+git rebase -i HEAD~n
+
+# --> git push -f
+git switch master
